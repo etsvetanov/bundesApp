2a6cc76144b3df6758ff9e3f23baf163e7d181f3
Evgeni Tsvetanov <ecvetanov@gmail.com>
2017-10-23 00:12:02 +0300

    Add <Menu /> and wire '/upcoming' to menu item

--- client/bundes-ui/src/App.js
+++ client/bundes-ui/src/App.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Menu} from 'semantic-ui-react';
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Route,
+    Link
 } from 'react-router-dom';
 
 
-import { UpcomingMatches } from './pages/upcomingMatches';
+import {UpcomingMatches} from './pages/upcomingMatches';
 
 
 class App extends Component {
-  render() {
-    return (
-        <Router>
-
-          <Route path="/upcoming" component={UpcomingMatches} />
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Menu stackable>
+                        <Menu.Item>
+                            <img src='http://www.camdensundayleague.com/wp-content/uploads/2016/08/Football-300x300.jpg'/>
+                        </Menu.Item>
+    
+                        <Menu.Item
+                            name="Upcoming"
+                        >
+                            <Link to="/upcoming"> Upcoming </Link>
+                        </Menu.Item>
+                    </Menu>
+                    <Route path="/upcoming" component={UpcomingMatches}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
