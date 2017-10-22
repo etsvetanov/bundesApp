8f5888ae581703b45bf5e4664f41265f6eb69301
Evgeni Tsvetanov <ecvetanov@gmail.com>
2017-10-22 23:46:37 +0300

    Render <UpcomingMatches /> on '/upcoming'

--- client/bundes-ui/src/App.js
+++ client/bundes-ui/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 import { UpcomingMatches } from './pages/upcomingMatches';
 
@@ -9,7 +15,10 @@ import { UpcomingMatches } from './pages/upcomingMatches';
 class App extends Component {
   render() {
     return (
-       <UpcomingMatches/>
+        <Router>
+
+          <Route path="/upcoming" component={UpcomingMatches} />
+        </Router>
     );
   }
 }
