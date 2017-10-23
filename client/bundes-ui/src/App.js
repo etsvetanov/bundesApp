4586bb4afc433397fabdc9fed4053e208948dc10
Evgeni Tsvetanov <etsvet@softserveinc.com>
2017-10-23 15:54:39 +0300

    Move Grid to <App />

--- client/bundes-ui/src/App.js
+++ client/bundes-ui/src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Menu} from 'semantic-ui-react';
+import { Menu, Grid } from 'semantic-ui-react';
 
 import {
-    BrowserRouter as Router,
-    Route,
-    Link
+   BrowserRouter as Router,
+   Route,
+   Link
 } from 'react-router-dom';
 
 
@@ -14,26 +14,39 @@ import {UpcomingMatches} from './pages/upcomingMatches';
 
 
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Menu stackable>
-                        <Menu.Item>
-                            <img src='http://www.camdensundayleague.com/wp-content/uploads/2016/08/Football-300x300.jpg'/>
-                        </Menu.Item>
-    
-                        <Menu.Item
-                            name="Upcoming"
-                        >
-                            <Link to="/upcoming"> Upcoming </Link>
-                        </Menu.Item>
-                    </Menu>
-                    <Route path="/upcoming" component={UpcomingMatches}/>
-                </div>
-            </Router>
-        );
-    }
+   render() {
+      return (
+         <Router>
+            <div>
+               <Menu stackable>
+                  <Menu.Item>
+                     <img src='http://www.camdensundayleague.com/wp-content/uploads/2016/08/Football-300x300.jpg'/>
+                  </Menu.Item>
+
+                  <Menu.Item
+                     name="Upcoming"
+                  >
+                     <Link to="/upcoming"> Upcoming </Link>
+                  </Menu.Item>
+               </Menu>
+               <Grid>
+                  <Grid.Row>
+                     <Grid.Column>
+                        <h1 className="text-center">
+                           Upcoming matches
+                        </h1>
+                     </Grid.Column>
+                  </Grid.Row>
+                  <Grid.Row centered>
+                     <Grid.Column mobile={16} largeScreen={14} widescreen={12}>
+                        <Route path="/upcoming" component={UpcomingMatches}/>
+                     </Grid.Column>
+                  </Grid.Row>
+               </Grid>
+            </div>
+         </Router>
+      );
+   }
 }
 
 export default App;
