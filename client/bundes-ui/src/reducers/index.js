9051a522fcac37eeaa2d6055331ec0dea777848f
Evgeni Tsvetanov <etsvet@softserveinc.com>
2017-11-01 11:20:46 +0200

    Add team and match reducers to the root reducer

--- client/bundes-ui/src/reducers/index.js
+++ client/bundes-ui/src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 
+import { match } from './match';
+import { team } from './team';
+
 
 const rootReducer = combineReducers({
-
+   match,
+   team,
 });
 
 export default rootReducer;
