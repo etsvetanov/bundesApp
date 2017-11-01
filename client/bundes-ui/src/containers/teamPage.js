7f9c0f93dedb59e4e57d5aed24e7146c6e6b881f
Evgeni Tsvetanov <etsvet@softserveinc.com>
2017-11-01 13:34:42 +0200

    Add team page

---
+++ client/bundes-ui/src/containers/teamPage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Statistic } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Loader } from 'semantic-ui-react';
+
+import { fetchMatches } from '../actions/match';
+import { getWinLossRationByTeamId, getUpcomingMatchesByTeam } from '../reducers/match';
+import { MatchList } from '../components/matchList';
+import { withRouter } from 'react-router-dom';
+
+
+class TeamPage extends React.Component {
+   componentDidMount() {
+      this.props.fetchMatches();
+   }
+
+   render() {
+      const { winLossRatios } = this.props;
+
+      if (!winLossRatios) {
+         return <Loader active inline='centered' />
+      }
+
+      return (
+         <div>
+            <div className="team-statistics-container">
+               <Statistic>
+                  <Statistic.Label>Wins</Statistic.Label>
+                  <Statistic.Value>{ winLossRatios.wins }</Statistic.Value>
+               </Statistic>
+               <Statistic>
+                  <Statistic.Label>Losses</Statistic.Label>
+                  <Statistic.Value>{ winLossRatios.losses }</Statistic.Value>
+               </Statistic>
+            </div>
+
+            <MatchList matches={this.props.upcomingMatches}/>
+         </div>
+      );
+   }
+}
+
+const mapStateToProps = (state, ownProps) => {
+   const teamId = ownProps.match.params.id;
+
+   return {
+      winLossRatios: getWinLossRationByTeamId(state, teamId),
+      upcomingMatches: getUpcomingMatchesByTeam(state, ownProps),
+   };
+};
+
+const mapDispatchToProps = {
+   fetchMatches,
+};
+
+TeamPage = withRouter(connect(mapStateToProps, mapDispatchToProps)(TeamPage));
+
+export { TeamPage };
