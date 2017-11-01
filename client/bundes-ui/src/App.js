9ea2b81e5ffb2b414bc586e87bb3f05d686e7fd6
Evgeni Tsvetanov <etsvet@softserveinc.com>
2017-11-01 16:23:47 +0200

    Remove menu render from <App/> and use <MenuContainer />
    Fix route components

--- client/bundes-ui/src/App.js
+++ client/bundes-ui/src/App.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Menu, Grid, Icon } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 import {
    BrowserRouter as Router,
    Route,
-   Link
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import { CurrentMatchdayPage, NextMatchdayPage, AStatisticsPage, SearchTeamPage } from './pages';
+import { CurrentMatchdayPage, NextMatchdayPage, StatisticsPage, TeamPage } from './containers';
+import { MenuContainer } from './containers/menuContainer';
 
 
 class App extends Component {
@@ -19,39 +18,7 @@ class App extends Component {
          <Provider store={this.props.store}>
             <Router>
                <div>
-                  <Menu stackable>
-
-                     <Menu.Item>
-                        <img src='http://www.camdensundayleague.com/wp-content/uploads/2016/08/Football-300x300.jpg'/>
-                     </Menu.Item>
-
-                     <Menu.Item
-                        name="current"
-                     >
-                        <Link to="/current"> Current </Link>
-                     </Menu.Item>
-
-                     <Menu.Item
-                        name="upcoming"
-                     >
-                        <Link to="/next"> Upcoming </Link>
-                     </Menu.Item>
-
-                     <Menu.Item
-                        name="statistics"
-                     >
-                        <Link to="/statistics"> Statistics </Link>
-                     </Menu.Item>
-
-                     <Menu.Item
-                        name="search"
-                     >
-                        <Link to="/search">
-                           <Icon name='search' size="big"/>
-                        </Link>
-                     </Menu.Item>
-                  </Menu>
-
+                  <MenuContainer/>
                   <Grid>
                      <Grid.Row>
                         <Grid.Column>
@@ -64,8 +31,8 @@ class App extends Component {
                         <Grid.Column mobile={16} largeScreen={14} widescreen={12}>
                            <Route path="/current" render={CurrentMatchdayPage} />
                            <Route path="/next" render={NextMatchdayPage} />
-                           <Route path="/statistics" render={AStatisticsPage} />
-                           <Route path="/search" component={SearchTeamPage} />
+                           <Route path="/statistics" component={StatisticsPage} />
+                           <Route path="/team/:id" component={TeamPage} />
                         </Grid.Column>
                      </Grid.Row>
                   </Grid>
